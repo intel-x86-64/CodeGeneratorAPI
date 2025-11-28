30694be8b162b04839da2df3aab34ccba23f8a05
intel-x86-64 <192.cpp@gmail.com>
2025-11-28 13:47:49 +0300

    Add class for Data Matrix barcode

---
+++ src/DataMatrixGenerator.cpp
@@ -0,0 +1,10 @@
+#include "../include/DataMatrixGenerator.h"
+
+std::string DataMatrixGenerator::generate(const std::string &text, int margin,
+                                          int size) {
+  writer.setMargin(margin);
+  writer.setEncoding(ZXing::CharacterSet::UTF8);
+  ZXing::BitMatrix matrix = writer.encode(text, size, size);
+
+  return ZXing::ToSVG(matrix);
+}
